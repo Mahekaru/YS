b2f18072b0694ddf378cb23000276b19d981d244
Michael Jennings <jenningsm@reninc.com>
2019-11-15 16:47:46 -0500

    Adding googleapi

--- YardSale/YardSale/Models/MapModel.cs
+++ YardSale/YardSale/Models/MapModel.cs
@@ -1,4 +1,7 @@
-﻿namespace YardSale.Models
+﻿
+using GoogleMapsApi.Entities.Common;
+
+namespace YardSale.Models
 {
 
     public class MapModel
@@ -6,11 +9,17 @@
         public string Address { get; set; }
         public double Latitude { get; set; }
         public double Longitude { get; set; }
+        public Location GeoCode { get; set; }
         public string Key { get; set; }
 
         public string GetAPIKey()
         {
             return "";
+        }
+
+        public Location GetGeoCode()
+        {
+
         }
     }
 }
